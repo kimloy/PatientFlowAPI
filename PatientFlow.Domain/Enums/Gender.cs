03051132c39e3a8d48f5afc65e324d1bf72a6c6f
Kimloy <p.changfatt@gmail.com>
2026-05-07 16:59:16 -0400

    Made the folder stucture for the domain project layer. Created the Base entity and the enum for gender

---
+++ PatientFlow.Domain/Enums/Gender.cs
@@ -0,0 +1,11 @@
+namespace PatientFlow.Domain.Enums;
+
+public enum Gender
+{
+    Unknown = 0,
+    Male = 1,
+    Female = 2,
+    Other = 3
+
+}
+
--- PatientFlow.Domain/obj/Debug/net8.0/PatientFlow.Domain.AssemblyInfo.cs
+++ PatientFlow.Domain/obj/Debug/net8.0/PatientFlow.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PatientFlow.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f405bc257cb11c8e04ff4bc9aeb10623cd60b248")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f508f50a22e9c9cf8673fb559fa176b8f863dcc9")]
 [assembly: System.Reflection.AssemblyProductAttribute("PatientFlow.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PatientFlow.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
